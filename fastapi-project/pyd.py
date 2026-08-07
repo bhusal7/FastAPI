c0204dde50e40ee345500b58fdf768aa31b31c1e
Vashudev Bhusal <pradeepbbusal@gmail.com>
2026-08-07 14:22:23 +0545

    Pydantic

---
+++ fastapi-project/pyd.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class LoanApplication(BaseModel):
+    name : str
+    age : int 
+    income : float
+    loan_amount : float
+    employment_years : int
+    
+@app.post("/predict")
+def predict_loan(application : LoanApplication):
+    
+    # model login
+    approved = (
+        application.income > 50000 and
+        application.employment_years > 2 and
+        application.age >= 21
+    )
+    
+    return {
+        "application_name" : application.name,
+        "loan_amount" : application.loan_amount,
+        "decision" : "approved" if approved else "rejected",
+        "review_income" : application.income
+    }
